9bcc30ad491e27c21d57e65744592066e0a889a0
Ivan Garcia <ivangm180@gmail.com>
2021-07-17 13:30:27 -0500

    Creacion y design components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 // Analog to createElement.
-import React from 'react';
+import React from "react";
 // Analog to appendChild.
-import ReactDOM from 'react-dom';
-import Badge from './components/Badge'
+import ReactDOM from "react-dom";
+import BadgeNew from "./pages/BadgeNew";
+import "bootstrap/dist/css/bootstrap.css";
+import "./global.css";
 
+const container = document.getElementById("app");
 
-const container = document.getElementById('app');
-
-// ReactDOM.render(__what__, __where__); 
-ReactDOM.render(<Badge />, container)
-
-
-
-
+// ReactDOM.render(__what__, __where__);
+// We've settled the props in Badge.
+ReactDOM.render(<BadgeNew />, container);
