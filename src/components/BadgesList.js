06f39d6d2124463812f43d56f9e392faba82dbe3
Ivan Garcia <ivangm180@gmail.com>
2021-07-26 09:28:03 -0500

    going into the project

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,15 +1,32 @@
 import React from "react";
 
+// Importing CSS Styles.
+import "./styles/BadgestList.css";
+
 class BadgesList extends React.Component {
   render() {
     return (
       <ul className="list-unstyled">
         {this.props.badges.map((badge) => {
           return (
-            <li key={badge.id}>
-              <p>
-                {badge.firstName} {badge.lastName}{" "}
-              </p>
+            <li key={badge.id} className="badges-item__container">
+              <div className="badges-item__avatar">
+                <img
+                  src={badge.avatarUrl}
+                  alt=""
+                  className="badges-item__image"
+                />
+              </div>
+              <div className="badges-item__user">
+                <p className="badges-item__name">
+                  {badge.firstName} {badge.lastName}{" "}
+                </p>
+                <div className="badges-item__twitter">
+                  <span className="twitterIcon"></span>
+                  <p>@{badge.twitter}</p>
+                </div>
+                <p className="badges-item__jobTitle">{badge.jobTitle}</p>
+              </div>
             </li>
           );
         })}
@@ -18,4 +35,4 @@ class BadgesList extends React.Component {
   }
 }
 
-export default BadgesList
+export default BadgesList;
