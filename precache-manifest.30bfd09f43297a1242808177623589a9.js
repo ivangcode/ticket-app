825f527222c0c84e8e6aaa79e4050ae976d199d1
Ivan Garcia <ivangm180@gmail.com>
2021-08-28 19:27:30 -0500

    Updates

---
+++ precache-manifest.30bfd09f43297a1242808177623589a9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e83e779eeb503379873",
+    "url": "/first-react-project/static/css/main.7eaedead.chunk.css"
+  },
+  {
+    "revision": "9e83e779eeb503379873",
+    "url": "/first-react-project/static/js/main.9e83e779.chunk.js"
+  },
+  {
+    "revision": "f40eae2edf191b226d0c",
+    "url": "/first-react-project/static/js/runtime~main.f40eae2e.js"
+  },
+  {
+    "revision": "d059fc00dcfca3e0d3d2",
+    "url": "/first-react-project/static/css/2.446a4825.chunk.css"
+  },
+  {
+    "revision": "d059fc00dcfca3e0d3d2",
+    "url": "/first-react-project/static/js/2.d059fc00.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/first-react-project/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/first-react-project/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/first-react-project/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/first-react-project/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "98d4d2856ed22bb56851871b50513536",
+    "url": "/first-react-project/static/media/facebook.98d4d285.svg"
+  },
+  {
+    "revision": "1390a4019607accb5ab4f8c0c7e42964",
+    "url": "/first-react-project/static/media/twitter.1390a401.svg"
+  },
+  {
+    "revision": "91618623aa2216e5720b81908f2ef2a4",
+    "url": "/first-react-project/static/media/instagram.91618623.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/first-react-project/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "01bcbda21eb8e46421189029b388d82e",
+    "url": "/first-react-project/static/media/elipse.01bcbda2.svg"
+  },
+  {
+    "revision": "3e61eabebf1a21ae354d96332c72fe1e",
+    "url": "/first-react-project/index.html"
+  }
+];
